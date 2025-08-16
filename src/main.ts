bdd1deb77389ea21733cd064e117b86f66a5d827
Matheus Lima <lima8matheus@gmail.com>
2025-08-16 12:06:23 -0300

    feat: display available currencies

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,19 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import { rates } from "./rates";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const fromSelect = document.getElementById("from") as HTMLSelectElement;
+const toSelect = document.getElementById("to") as HTMLSelectElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function populateSelect(select: HTMLSelectElement) {
+  Object.keys(rates).forEach((currency) => {
+    const option = document.createElement("option");
+    option.value = currency;
+    option.textContent = currency;
+    select.appendChild(option);
+  });
+}
+
+populateSelect(fromSelect);
+populateSelect(toSelect);
+fromSelect.value = "USD";
+toSelect.value = "BRL";
